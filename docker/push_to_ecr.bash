60c5d2b1c093df4a6739746864d8157fa497ea7b
Robert Markl <robert.markl@bmw.de>
2025-08-07 16:11:56 +0200

    DevContainer: Initial Commit

---
+++ docker/push_to_ecr.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -ex
+
+aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 799634405166.dkr.ecr.eu-central-1.amazonaws.com
+docker build -t coder/agentic-ai-nano .
+docker tag coder/agentic-ai-nano:latest 799634405166.dkr.ecr.eu-central-1.amazonaws.com/coder/agentic-ai-nano:latest
+docker push 799634405166.dkr.ecr.eu-central-1.amazonaws.com/coder/agentic-ai-nano:latest
