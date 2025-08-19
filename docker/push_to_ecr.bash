8c06725af160b49bf180a7d47f3d8693a5c0bf2a
Robert Markl <robert.markl@bmw.de>
2025-08-19 15:53:20 +0200

    bootstrap

--- docker/push_to_ecr.bash
+++ docker/push_to_ecr.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -ex
+set -exuo pipefail
 
 aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 799634405166.dkr.ecr.eu-central-1.amazonaws.com
 docker build -t coder/agentic-ai-nano .
